5b8df8d69909a5eeb83730479c4e3c4fb999b3e3
george <georgebenett@gmail.com>
2024-11-20 00:07:52 +0100

    fixing previous commit

--- ble_spp_client/main/adc.h
+++ ble_spp_client/main/adc.h
@@ -1,10 +1,11 @@
 #ifndef ADC_H
 #define ADC_H
 
+#include <stdint.h>
 #include "esp_err.h"
 
 esp_err_t adc_init(void);
-int adc_read_value(void);
+int32_t adc_read_value(void);
 void adc_start_task(void);
 
 #endif // ADC_H 
